ee35368563dbfccc621b4f9665aeb709f630a490
siftniloy <sifatniloy18@gmail.com>
2022-03-28 11:03:54 +0600

    added local storage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header' >
-            <h1>This is header</h1>
+
             <img className='logo' src={logo} alt="" />
             <nav>
                 <a href="/shop">shop</a>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -7,36 +8,59 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
+        console.log('product api called')
         fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data);
+                console.log('products received')
+            })
 
     }, [])
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
-        setCart(newCart);
-    }
-    return (
-        <div className='shop-container'>
-            <div className="product-container">
-                <h3>Products:{products.length} </h3>
-                {
-                    products.map(product =>
-                        <Product
-                            key={product.key}
-                            product={product}
-                            handleAddToCart={handleAddToCart}>
+    useEffect(() => {
+        console.log('local storage called')
+        if (products.length) {
+            const savedCart = getStoredCart();
+            const storedCart = [];
+            for (const key in savedCart) {
+                const addedProduct = products.find(product => product.key === key);
+                
+                storedCart.push(addedProduct);
+            }
+            setCart(storedCart);
+        }
 
-                        </Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
+    }, [products]);
 
 
+
+
+const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.key);
+}
+return (
+    <div className='shop-container'>
+        <div className="product-container">
+            <h3>Products:{products.length} </h3>
+            {
+                products.map(product =>
+                    <Product
+                        key={product.key}
+                        product={product}
+                        handleAddToCart={handleAddToCart}>
+
+                    </Product>)
+            }
         </div>
-    );
+        <div className="cart-container">
+            <Cart cart={cart}></Cart>
+        </div>
+
+
+    </div>
+);
 };
 
 export default Shop;
